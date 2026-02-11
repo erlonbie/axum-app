5b89107db4f1c16e3da3889950e10f6b872e858b
Erlon Bié <erlon.bie@eldorado.org.br>
2026-02-11 16:38:39 -0400

    feat: add action_logs migration

--- crates/migration/src/lib.rs
+++ crates/migration/src/lib.rs
@@ -1,10 +1,13 @@
 pub use sea_orm_migration::prelude::*;
+
 mod common;
 mod m20220101_000001_create_user;
 mod m20260211_021635_create_posts;
 mod m20260211_175842_oauth_providers;
 mod m20260211_182039_oauth_connections;
 mod m20260211_182412_action_resource_type_enum;
+mod m20260211_182635_create_action_logs;
+
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -16,6 +19,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20260211_175842_oauth_providers::Migration),
             Box::new(m20260211_182039_oauth_connections::Migration),
             Box::new(m20260211_182412_action_resource_type_enum::Migration),
+            Box::new(m20260211_182635_create_action_logs::Migration),
         ]
     }
 }
