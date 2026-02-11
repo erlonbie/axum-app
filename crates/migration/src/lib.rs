6f068b358af4b30c6d58b826415afa187436f655
Erlon Bié <erlon.bie@eldorado.org.br>
2026-02-11 16:33:51 -0400

    feat: add posts migration

--- crates/migration/src/lib.rs
+++ crates/migration/src/lib.rs
@@ -1,5 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_create_user;
+mod m20260211_021635_create_posts;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -7,6 +8,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_create_user::Migration),
+            Box::new(m20260211_021635_create_posts::Migration),
         ]
     }
 }
