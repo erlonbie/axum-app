5d085c9e1174233d3527dd1f1b31f6e3b1d0f4d1
Erlon Bié <erlon.bie@eldorado.org.br>
2026-02-12 09:13:23 -0400

    chore: add server crate

---
+++ crates/server/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(handler));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
+}
